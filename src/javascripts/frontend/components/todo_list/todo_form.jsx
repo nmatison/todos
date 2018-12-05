3679fae7c0bf451a58c5a434ff57eed6bd80015f
Nick <nmatison@gmail.com>
2018-12-05 17:13:24 -0500

    Add submit option for form

--- src/javascripts/frontend/components/todo_list/todo_form.jsx
+++ src/javascripts/frontend/components/todo_list/todo_form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { uniqueId } from '../../util/unique_id_util';
 
 class TodoForm extends React.Component {
 
@@ -11,6 +12,7 @@ class TodoForm extends React.Component {
       done: false
     };
     this.updateField = this.updateField.bind(this);
+    this.submitForm = this.submitForm.bind(this);
   };
 
   updateField(field) {
@@ -19,15 +21,30 @@ class TodoForm extends React.Component {
     };
   }
 
+  submitForm(e) {
+    e.preventDefault();
+    const todo = Object.assign({}, this.state, {id: uniqueId()})
+    this.props.receiveTodo(todo);
+    this.setState = {
+      id: null,
+      title: "",
+      body: "",
+      done: false
+    };
+  }
+
   render() {
     return (
-      <div className="todo-form">
-        <label> Title
-          <input type="text" onChange={this.updateField("title")} value={this.state.title}/>
-        </label>
-        <label> Description
-          <input type="text" onChange={this.updateField("body")} value={this.state.body} />
-        </label>
+      <div className="form-div">
+        <form className="todo-form" onSubmit={this.submitForm}>
+          <label> Title
+            <input type="text" onChange={this.updateField("title")} value={this.state.title}/>
+          </label>
+          <label> Description
+            <input type="text" onChange={this.updateField("body")} value={this.state.body} />
+          </label>
+          <input type="submit" value="Submit Todo"/>
+        </form>
       </div>
     );
   };
